8db81738512f5e8027cf8c45440ce53ed7575afd
Ajinkya-B <ajinkya.bhosale85@gmail.com>
2023-09-04 22:56:07 -0400

    Almost finished home page and added about page

--- src/App.js
+++ src/App.js
@@ -1,29 +1,26 @@
 import "./App.css";
-import { useState } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import AppNavbar from "./components/AppNavbar";
 import Home from "./components/Home";
+import SidebarImage from "./components/SidebarImage";
 
 function App() {
-  const [rotate, setRotate] = useState(0);
   return (
     <div className="app-container">
       <div className="app">
         <AppNavbar />
         <div className="page-content-container">
           <div className="page-content">
-            <div className="bg-image">
-              <img
-                style={{
-                  transform: `rotate(${rotate}deg)`,
-                  transformOrigin: "right top",
-                }}
-                src="https://th.bing.com/th/id/R.a955733fe7dd92597a9857ddc0b108ee?rik=tbkv76DsQRdfHw&riu=http%3a%2f%2fclipart-library.com%2fimages_k%2fcartoon-earth-transparent%2fcartoon-earth-transparent-15.png&ehk=N6lnTsE6EF57J45C%2bMhy5q9RqA46SHhlO6dSKqxgYYA%3d&risl=1&pid=ImgRaw&r=0"
-                alt="BackgroundImage"
-              />
-            </div>
+            <SidebarImage />
             <Home />
-            <div className="dummy" style={{ height: "1000px" }}></div>
+            <div
+              id="about"
+              className="dummy"
+              style={{
+                height: "100%",
+                backgroundColor: "#F7F1E1",
+              }}
+            ></div>
           </div>
         </div>
       </div>
--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,17 +1,24 @@
 import React from "react";
 import { TypeAnimation } from "react-type-animation";
 import { ImMail2 } from "react-icons/im";
+import { TbSquareRoundedChevronDownFilled } from "react-icons/tb";
 
 export default function Home() {
   return (
-    <div className="content-2" style={{ height: "100%" }}>
+    <div
+      className="content-2"
+      style={{
+        height: "100%",
+        backgroundColor: "#eff8e2",
+      }}
+    >
       <div
         style={{
           display: "flex",
           alignItems: "center",
-          justifyContent: "center",
           flexDirection: "column",
           height: "100%",
+          flex: "1",
         }}
       >
         <div
@@ -19,18 +26,25 @@ export default function Home() {
             display: "flex",
             justifyContent: "center",
             alignItems: "center",
+            marginTop: "auto",
           }}
         >
-          <h2 class="text_shadows">HEY</h2>
-
+          <h2 class="text_shadows">HEY! </h2>
           <div style={{ fontSize: "2.5em" }}>My name is Ajinkya.</div>
         </div>
+
         <div style={{ marginBottom: "10px" }}>
           <span style={{ fontSize: "1.5em" }}>I'm a</span>
           <TypeAnimation
-            sequence={[" developer", 1000, " leader", 1000]}
+            sequence={[" developer", 1000, " leader", 1000, " innovator", 1000]}
             speed={50}
-            style={{ whiteSpace: "pre-line", fontSize: "1.5em" }}
+            style={{
+              whiteSpace: "pre-line",
+              fontSize: "1.5em",
+              backgroundColor: "#f6aca2",
+              fontWeight: "600",
+              borderRadius: "10px",
+            }}
             repeat={Infinity}
           />
         </div>
@@ -47,6 +61,24 @@ export default function Home() {
           <ImMail2 style={{ marginRight: "10px" }} />
           GET IN TOUCH
         </span>
+        <a
+          href="#about"
+          style={{
+            textDecoration: "none",
+            marginTop: "auto",
+            marginBottom: "20px",
+          }}
+        >
+          <TbSquareRoundedChevronDownFilled
+            style={{
+              width: "3em",
+              height: "3em",
+              color: "black",
+              transition: "all 0.3s ease-in-out",
+            }}
+            className="scroll-down"
+          />
+        </a>
       </div>
     </div>
   );
