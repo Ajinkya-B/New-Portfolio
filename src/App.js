7a18a09f4616283e6a594ddd146e6de30bd4c57e
Ajinkya-B <ajinkya.bhosale85@gmail.com>
2023-09-07 19:23:52 -0400

    Added section for work/club experience

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Container from "react-bootstrap/Container";
+import Col from "react-bootstrap/Col";
+import Nav from "react-bootstrap/Nav";
+import Row from "react-bootstrap/Row";
+import Tab from "react-bootstrap/Tab";
 import AppNavbar from "./components/AppNavbar";
 import Home from "./components/Home";
 import SidebarImage from "./components/SidebarImage";
@@ -139,6 +143,146 @@ function App() {
               </div>
               <div style={{ width: "60%", marginTop: "100px" }}>
                 <span className="demo-1">Where I've worked</span>
+                <Tab.Container id="left-tabs-example" defaultActiveKey="first">
+                  <Row>
+                    <Col sm={3}>
+                      <Nav variant="pills" className="flex-column">
+                        <Nav.Item>
+                          <Nav.Link eventKey="first">WSP Canada</Nav.Link>
+                        </Nav.Item>
+                        <Nav.Item>
+                          <Nav.Link eventKey="second">
+                            UofT Seismic Design...
+                          </Nav.Link>
+                        </Nav.Item>
+                        <Nav.Item>
+                          <Nav.Link eventKey="third">HIPPOS Software</Nav.Link>
+                        </Nav.Item>
+                      </Nav>
+                    </Col>
+                    <Col sm={9}>
+                      <Tab.Content>
+                        <Tab.Pane eventKey="first">
+                          <div className="hSMFmU fade-enter-done">
+                            <h3>
+                              <span>Software Engineer</span>
+                              <span className="company">
+                                &nbsp;@&nbsp;
+                                <a
+                                  href="https://www.jet-contractors.com/"
+                                  className="inline-link"
+                                  rel="noopener noreferrer"
+                                  target="_blank"
+                                >
+                                  WSP Canada
+                                </a>
+                              </span>
+                            </h3>
+                            <p className="range">Mar 2020 - Present</p>
+                            <div>
+                              <ul>
+                                <li>
+                                  Developing Web applications with Spring
+                                  Framework
+                                </li>
+                                <li>
+                                  Maintain the performance of applications
+                                  developed using spring boot and angular
+                                </li>
+                                <li>
+                                  Refactoring the code of a scalable web
+                                  application from Angularjs to Angular
+                                </li>
+                                <li>
+                                  Collaborate with the UX/UI team to design and
+                                  develop a REST API
+                                </li>
+                              </ul>
+                            </div>
+                          </div>
+                        </Tab.Pane>
+                        <Tab.Pane eventKey="second">
+                          <div className="hSMFmU fade-enter-done">
+                            <h3>
+                              <span>Software Engineer</span>
+                              <span className="company">
+                                &nbsp;@&nbsp;
+                                <a
+                                  href="https://www.jet-contractors.com/"
+                                  className="inline-link"
+                                  rel="noopener noreferrer"
+                                  target="_blank"
+                                >
+                                  UofT Seismic Design Team
+                                </a>
+                              </span>
+                            </h3>
+                            <p className="range">Mar 2020 - Present</p>
+                            <div>
+                              <ul>
+                                <li>
+                                  Developing Web applications with Spring
+                                  Framework
+                                </li>
+                                <li>
+                                  Maintain the performance of applications
+                                  developed using spring boot and angular
+                                </li>
+                                <li>
+                                  Refactoring the code of a scalable web
+                                  application from Angularjs to Angular
+                                </li>
+                                <li>
+                                  Collaborate with the UX/UI team to design and
+                                  develop a REST API
+                                </li>
+                              </ul>
+                            </div>
+                          </div>
+                        </Tab.Pane>
+                        <Tab.Pane eventKey="third">
+                          <div className="hSMFmU fade-enter-done">
+                            <h3>
+                              <span>Software Engineer</span>
+                              <span className="company">
+                                &nbsp;@&nbsp;
+                                <a
+                                  href="https://www.jet-contractors.com/"
+                                  className="inline-link"
+                                  rel="noopener noreferrer"
+                                  target="_blank"
+                                >
+                                  HIPPOS Software
+                                </a>
+                              </span>
+                            </h3>
+                            <p className="range">Mar 2020 - Present</p>
+                            <div>
+                              <ul>
+                                <li>
+                                  Developing Web applications with Spring
+                                  Framework
+                                </li>
+                                <li>
+                                  Maintain the performance of applications
+                                  developed using spring boot and angular
+                                </li>
+                                <li>
+                                  Refactoring the code of a scalable web
+                                  application from Angularjs to Angular
+                                </li>
+                                <li>
+                                  Collaborate with the UX/UI team to design and
+                                  develop a REST API
+                                </li>
+                              </ul>
+                            </div>
+                          </div>
+                        </Tab.Pane>
+                      </Tab.Content>
+                    </Col>
+                  </Row>
+                </Tab.Container>
               </div>
             </div>
           </div>
